42f75b02584a753a697a7f66da86377d005c6506
sudeepthinittala <17pa1a05b5@vishnu.edu.in>
2020-03-11 09:58:15 +0530

    first commit

---
+++ patterns/src/main/java/com/epam/design/patterns/Adapter.java
@@ -0,0 +1,53 @@
+package com.epam.design.patterns;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public interface Adapter {
+
+	static final Logger LOGGER=LogManager.getLogger(App.class);
+
+	    public void fly(); 
+	    public void makeSound(); 
+}
+	  
+	class Sparrow implements Adapter
+	{ 
+	    public void fly() 
+	    { 
+	    	LOGGER.info("Flying"); 
+	    } 
+	    public void makeSound() 
+	    { 
+	        LOGGER.info("Chirp Chirp"); 
+	    } 
+	} 
+	  
+	interface ToyDuck 
+	{ 
+	    static final Logger LOGGER=LogManager.getLogger(App.class);
+
+	    public void squeak(); 
+	} 
+	  
+	class PlasticToyDuck implements ToyDuck 
+	{ 
+	    public void squeak() 
+	    { 
+	        LOGGER.info("Squeak"); 
+	    } 
+	} 
+	  
+	class BirdAdapter implements ToyDuck 
+	{  
+	    Adapter bird; 
+	    public BirdAdapter(Adapter bird) 
+	    { 
+	        
+	        this.bird = bird; 
+	    } 
+	  
+	    public void squeak() 
+	    { 
+	        bird.makeSound(); 
+	    } 
+	} 
+
---
+++ patterns/src/main/java/com/epam/design/patterns/Singleton.java
@@ -0,0 +1,21 @@
+package com.epam.design.patterns;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class Singleton 
+{ 
+	protected
+	static final Logger LOGGER=LogManager.getLogger(App.class);
+
+    private static Singleton obj;
+    private Singleton(){
+    	LOGGER.info("constructor of singleton ");
+    } 
+  
+    public static Singleton getInstance() 
+    { 
+        if (obj==null) 
+            obj = new Singleton(); 
+        return obj; 
+    } 
+} 
